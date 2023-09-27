bff35cdd0aea24f97a82be65babd2e0976b78571
SeasonLeague <facebookups23@gmail.com>
2023-09-27 05:56:39 +0100

    editing files

--- js/script.js
+++ js/script.js
@@ -6,34 +6,27 @@ var typed = new Typed(".typing", {
     "a HR Manager",
     "a Problem Solver",
     "a Fierce Young Lady"],
-    typeSpeed:100,
+    typeSpeed:150,
     BackSpeed:60,
     loop:true
-})
-
-
-// Initialize Email.js with your credentials
-emailjs.init("service_qnsges7");
-
-const contactForm = document.getElementById("contact-form");
-
-contactForm.addEventListener("click", function (event) {
-  event.preventDefault();
-
-  // Get the form data
-  const formData = new FormData(contactForm);
-
-  // Send the email using Email.js
-  emailjs
-    .sendForm("service_qnsges7", "template_ax6yuvq", formData, "lfk0reaWA4_Au5Odp")
-    .then(
-      function () {
-        alert("Form submitted successfully!");
-        contactForm.reset();
-      },
-      function (error) {
-        console.error("Form submission failed:", error);
-        alert("Form submission failed. Please try again later.");
-      }
-    );
 });
+
+
+
+emailjs.init('lfk0reaWA4_Au5Odp'); // Replace with your User ID from EmailJS
+
+function sendEmail() {
+    const to = document.getElementById("to").value;
+    const subject = document.getElementById("subject").value;
+    const message = document.getElementById("message").value;
+
+    emailjs.send('service_qnsges7', 'template_ax6yuvq', {
+        to_email: to,
+        subject: subject,
+        message: message
+    }).then(function(response) {
+        alert("Email sent successfully:", response);
+    }, function(error) {
+        alert("Email sending failed:", error);
+    });
+}
